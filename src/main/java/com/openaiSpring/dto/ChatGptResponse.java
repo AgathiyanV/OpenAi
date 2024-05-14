0aed5fbe23504071359ae4d5a86c690dd38b29a3
AgathiyanV <agathiya.v.cse.2019@snsct.org>
2024-05-14 09:45:45 +0530

    first commit

---
+++ src/main/java/com/openaiSpring/dto/ChatGptResponse.java
@@ -0,0 +1,73 @@
+package com.openaiSpring.dto;
+
+
+import java.util.List;
+
+
+public class ChatGptResponse {
+
+    private List<Choice> choices;
+    
+
+   
+    @Override
+	public String toString() {
+		return "ChatGptResponse [choices=" + choices + "]";
+	}
+
+
+
+	public List<Choice> getChoices() {
+		return choices;
+	}
+
+
+
+	public void setChoices(List<Choice> choices) {
+		this.choices = choices;
+	}
+
+
+
+	public ChatGptResponse(List<Choice> choices) {
+		super();
+		this.choices = choices;
+	}
+
+
+
+	public static class Choice {
+
+        private int index;
+        private Message message;
+		public Choice() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		public Choice(int index, Message message) {
+			super();
+			this.index = index;
+			this.message = message;
+		}
+		public int getIndex() {
+			return index;
+		}
+		public void setIndex(int index) {
+			this.index = index;
+		}
+		public Message getMessage() {
+			return message;
+		}
+		public void setMessage(Message message) {
+			this.message = message;
+		}
+		@Override
+		public String toString() {
+			return "Choice [index=" + index + ", message=" + message + "]";
+		}
+        
+
+    }
+    
+
+}
---
+++ src/test/java/com/openaiSpring/ChatgptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openaiSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
